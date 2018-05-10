38ca9df1d22173d17d5ed0d3b72cc0dd7de899ab
Karol <karol.wawer.pl@gmail.com>
2018-05-10 12:19:08 +0200

    dodanie pliku main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+function dzialania(a, b) {
+    var a = 55;
+    var b = 55;
+    var suma = a+b;
+    console.log("Wynik dodawania wynosi " +suma);
+    var roznica = a-b;
+    if (a <b) {
+        console.log("Wynik odejmowania jest ujemny i wynosi " +roznica);
+    } else {
+        console.log("Wynik odejmowania wynosi " +roznica);
+    }
+    var iloczyn = a*b;
+    console.log("Wynik mnożenia wynosi " +iloczyn);
+    
+} dzialania();
